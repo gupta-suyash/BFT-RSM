c193f16ed68e727923958577f642617c2d2ead76
gupta-suyash <ubuntu@ip-172-31-88-62.ec2.internal>
2022-08-09 20:30:15 +0000

    globals

--- Code/system/parser.cpp
+++ Code/system/parser.cpp
@@ -2,9 +2,10 @@
 
 void parser(int argc, char *argv[])
 {
+	cout << "Argc: " << argc << endl;
 	for (int i = 1; i < argc; i++)
 	{
-		if (argv[i][1] == 'n' && argv[i][2] == 'i' && argv[i][3] == 'd'){
+		if (argv[i][0] == 'n' && argv[i][1] == 'i' && argv[i][2] == 'd'){
 			cout << "inside:" << i << endl;
 			g_node_id = atoi(&argv[i][4]);
 			g_rsm_id = g_node_id / g_nodes_rsm;
