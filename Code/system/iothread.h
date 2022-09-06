140e7bf0165fe5bf54330ce76bfd5d0871f16103
gupta-suyash <sugupta@ucdavis.edu>
2022-09-06 06:57:48 +0000

    iothreading structure inheritance

--- Code/system/iothread.h
+++ Code/system/iothread.h
@@ -5,31 +5,31 @@
 #include "global.h"
 #include "pipe_queue.h"
 
-//class IOThreads {
-//public:
-//	UInt16 thd_id_; // Thread id.
-//	thread thd_;	// Thread.
-//	virtual void Init(UInt16 thd_id) = 0;
-//	virtual void Run() = 0;
-//	virtual UInt16 GetThreadId() = 0;
-//};	
-
-
-// Threads that send or receive messages from other RSM.
-class SendThread {
+class IOThreads {
 public:
 	UInt16 thd_id_; // Thread id.
 	thread thd_;	// Thread.
+	virtual void Init(UInt16 thd_id) = 0;
+	virtual void Run() = 0;
+	virtual UInt16 GetThreadId() = 0;
+};	
+
+
+// Threads that send or receive messages from other RSM.
+class SendThread : public IOThreads {
+public:
+	//UInt16 thd_id_; // Thread id.
+	//thread thd_;	// Thread.
 	void Init(UInt16 thd_id);
 	void Run();
 	UInt16 GetThreadId();
 };	
 
 
-class RecvThread {
+class RecvThread : public IOThreads {
 public:
-	UInt16 thd_id_; // Thread id.
-	thread thd_;	// Thread.
+	//UInt16 thd_id_; // Thread id.
+	//thread thd_;	// Thread.
 	void Init(UInt16 thd_id);
 	void Run();
 	UInt16 GetThreadId();
