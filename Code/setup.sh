37421a55a3e9d155ea40cbc516087ce0553183cc
mmurray22 <michaelamurray2357@gmail.com>
2022-09-06 13:46:24 -0700

    Successfully running tests in the Code directory

--- Code/setup.sh
+++ Code/setup.sh
@@ -1,23 +1,31 @@
 #!/bin/sh
 
 # ninja install
-echo "shell command working!"
+echo "shell command starting!"
 apt-get update
 apt-get upgrade
+echo "shell updated and upgraded"
+apt-get install  libboost-all-dev
 apt-get -y install cmake
 apt -y install build-essential
 apt-get -y install re2c
+echo "basic packages installed"
 gh repo clone ninja-build/ninja
+echo "attempt to clone ninja successful"
 cd ninja
 cmake -Bbuild-cmake
 cmake --build build-cmake
+echo "built ninja"
 
 # nng installation
-tar -xzf ./Code/nng-1.5.2.tar.gz
+tar -xzf ./nng-1.5.2.tar.gz
+echo "untar of nng successful"
 cd nng-1.5.2
 mkdir build
 cd build
+echo "build directory creation successful"
 cmake -G Ninja ..
 ninja
 ninja test
 ninja install
+echo "script is successful!"
