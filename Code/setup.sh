8250495a75608c794150d4fe55d76943df79d546
mmurray22 <michaelamurray2357@gmail.com>
2022-09-06 14:39:07 -0700

    Final attempt to fix nng directory issues, then pivoting

--- Code/setup.sh
+++ Code/setup.sh
@@ -20,9 +20,9 @@ echo $PWD
 echo "built ninja"
 
 # nng installation
-tar -xzf ./nng-1.5.2.tar.gz
+tar -xzf ./Code/nng-1.5.2.tar.gz
 echo "untar of nng successful"
-cd nng-1.5.2
+cd ./Code/nng-1.5.2
 mkdir build
 cd build
 echo "build directory creation successful"
