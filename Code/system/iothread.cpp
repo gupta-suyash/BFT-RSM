8c19ce8debfcfad803a228a0fa24557a577e2a1d
murray22 <michaelamurray2357@gmail.com>
2023-03-07 19:09:20 -0700

    One to one initial impl. Not tested

--- Code/system/iothread.cpp
+++ Code/system/iothread.cpp
@@ -175,6 +175,52 @@ void runAllToAllSendThread(const std::shared_ptr<iothread::MessageQueue> message
     SPDLOG_INFO("ALL PACKETS SENT (PRESUMABLY)");
 }
 
+void runOneToOneSendThread(const std::shared_ptr<iothread::MessageQueue> messageInput, 
+		           const std::shared_ptr<Pipeline> pipeline,
+                           const std::shared_ptr<Acknowledgment> acknowledgment,
+                   	   const std::shared_ptr<AcknowledgmentTracker> ackTracker,
+                   	   const std::shared_ptr<QuorumAcknowledgment> quorumAck, 
+			   const NodeConfiguration configuration)
+{
+    SPDLOG_INFO("Send Thread starting with TID = {}", gettid());
+    constexpr auto kSleepTime = 1ns;
+    const auto kResendWaitPeriod = 5s;
+    const auto &[kOwnNetworkSize, kOtherNetworkSize, kOwnMaxNumFailedNodes, kOtherMaxNumFailedNodes, kNodeId,
+                 kLogPath, kWorkingDir] = configuration;
+    const auto kMaxMessageSends = kOwnMaxNumFailedNodes + kOtherMaxNumFailedNodes + 1;
+
+    // auto sendMessageBuffer = std::vector<scrooge::CrossChainMessage>{};
+    size_t num_packets = 0;
+
+    while (num_packets < get_number_of_packets())
+    {
+        // Send and store new messages
+        // TODO Benchmark if it is better to empty the queue sending optimistically or retry first
+        // TODO Implement multithreaded sending to parallelize sending messages (or does this matter w sockets?)
+        scrooge::CrossChainMessage newMessage;
+        while (messageInput->pop(newMessage))
+        {
+            num_packets += 1;
+            const auto sequenceNumber = newMessage.data().sequence_number();
+            const auto originalSenderId = sequenceNumber % kOwnNetworkSize;
+
+            if (originalSenderId == kNodeId)
+            {
+                // sendMessageBuffer.emplace_back(std::move(newMessage));
+                const auto receiverNode =
+                    getMessageDestinationId(sequenceNumber, kNodeId, kOwnNetworkSize, kOtherNetworkSize);
+
+                // stats.startTimer(sequenceNumber);
+                pipeline->SendToOtherRsm(kNodeId, std::move(newMessage));
+                continue;
+            }
+        }
+    }
+
+    // stats.printOutAllResults();
+    SPDLOG_INFO("ALL PACKETS SENT (PRESUMABLY)");
+}
+
 void runSendThread(const std::shared_ptr<iothread::MessageQueue> messageInput, const std::shared_ptr<Pipeline> pipeline,
                    const std::shared_ptr<Acknowledgment> acknowledgment,
                    const std::shared_ptr<AcknowledgmentTracker> ackTracker,
