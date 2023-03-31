fe561a6c4a7cda60801be0ec08d4ce63bd5e9ebd
Reginald Frank <reginaldfrank77@berkeley.edu>
2023-03-31 11:37:44 -0600

    Fix go getting added to path

--- Code/setup_local.sh
+++ Code/setup_local.sh
@@ -69,16 +69,11 @@ ldconfig
 cd ..
 
 # Go 1.20 install
-
 sudo wget https://go.dev/dl/go1.20.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
 sudo rm -rf go1.20.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
-echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
-echo "export GOPATH=$HOME/go" >> $HOME/.profile
-echo "export PATH=$PATH:$GOROOT/bin:$GOPATH/bin" >> $HOME/.profile
+echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.profile
 source $HOME/.profile
 
 # Insatall go protoc extension
