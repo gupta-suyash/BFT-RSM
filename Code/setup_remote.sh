3bcc7f1db12bd1f40302e6110137eca17f3fc6a0
Reginald Frank <reginaldfrank77@berkeley.edu>
2023-03-31 22:44:20 -0600

    Add gopath to path correctly

--- Code/setup_remote.sh
+++ Code/setup_remote.sh
@@ -68,6 +68,8 @@ sudo tar -C /usr/local -xzf go1.20.linux-amd64.tar.gz
 sudo rm -rf go1.20.linux-amd64.tar.gz
 echo "export PATH=\$PATH:/usr/local/go/bin" >> $HOME/.profile
 source $HOME/.profile
+echo "export PATH=\$PATH:`go env GOPATH`/bin" >> $HOME/.profile
+source $HOME/.profile
 
 # Insatall go protoc extension
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
