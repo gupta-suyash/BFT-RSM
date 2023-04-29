004d198d924a1631259028db870f32791a4d3a8c
murray22 <michaelamurray2357@gmail.com>
2023-04-29 14:38:28 -0600

    Can now automatically start all algorand nodes

--- Code/experiments/experiment_scripts/participation.sh
+++ Code/experiments/experiment_scripts/participation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export ALGORAND_DATA=/proj/ove-PG0/murray/node/n$1
-goal node restart -p "10.10.1.11:4161"
-node wallet_test.js /proj/ove-PG0/murray/go-algorand/wallet_app/node$1.json test$1
+echo $ALGORAND_DATA
+~/go/bin/goal node restart -p "10.10.1.11:4161"
+node /proj/ove-PG0/murray/go-algorand/wallet_app/wallet_test.js /proj/ove-PG0/murray/go-algorand/wallet_app/node$1.json test$1
 
--- Code/experiments/experiment_scripts/run_algorand.py
+++ Code/experiments/experiment_scripts/run_algorand.py
@@ -4,6 +4,10 @@ import os
 import subprocess
 import threading
 import socket
+import datetime
+import time
+import random
+import multiprocessing
 import concurrent.futures
 
 setup_dir = os.path.realpath(os.path.dirname(__file__))
@@ -12,24 +16,35 @@ from ssh_util import *
 
 from experiment_fxns import *
 
-participation_list = ["10.10.1.5", "10.10.1.4", "10.10.1.3", "10.10.1.2", "10.10.1.9", "10.10.1.8", "10.10.1.7", "10.10.1.6", "10.10.1.10", "10.10.1.12"]
+hosts = ["10.10.1.12", "10.10.1.2", "10.10.1.3", "10.10.1.4", "10.10.1.5", "10.10.1.6", "10.10.1.7", "10.10.1.8", "10.10.1.9", "10.10.1.10" ]
 # relay = ["10.10.1.11"]
 
 def main():
-    relay_cmd = "source ./relay.sh"
-    subprocess.call(relay_cmd)
+    relay_cmd = ". /proj/ove-PG0/murray/BFT-RSM/Code/experiments/experiment_scripts/relay.sh"
+    executeCommand(relay_cmd)
 
 
     hostname=socket.gethostname()   
-    IPAddr=socket.gethostbyname(hostname)  
-    for i in range(0, len(participation_list)):
-        cmd = "source ./participation.sh " + i
-        if participation_list[i] == IPAddr:
+    IPAddr=socket.gethostbyname(hostname) 
+    print(hosts) 
+    ssh_command_list = []
+    thread_list = list()
+    for i in range(0, len(hosts)):
+        cmd = ". /proj/ove-PG0/murray/BFT-RSM/Code/experiments/experiment_scripts/participation.sh " + str(i)
+        cmd_ssh = "ssh -o StrictHostKeyChecking=no -t " + hosts[i] + " '" + cmd + "'"
+        ssh_command_list.append(cmd_ssh)
+    for i in range(0, len(hosts)):
+        print("Host: ", hosts[i])
+        hostname=socket.gethostname()
+        IPAddr=socket.gethostbyname(hostname)
+        if hosts[i] == IPAddr:
             print("Host is: ", IPAddr)
             continue
-        cmd = "ssh -o StrictHostKeyChecking=no -t " + participation_list[i] + " '" + cmd + "'"
-        subprocess.check_call(cmd, shell=True)
-    run()
-
+        t = threading.Thread(target=executeCommand, args=(ssh_command_list[i],))
+        thread_list.append(t)
+    for t in thread_list:
+        t.start()
+    for t in thread_list:
+        t.join()
 if __name__ == "__main__":
     main()
