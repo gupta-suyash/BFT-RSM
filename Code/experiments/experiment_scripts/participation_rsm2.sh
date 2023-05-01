f5221820745125c2671bcfb7e091066fd23e924d
mmurray22 <michaelamurray2357@gmail.com>
2023-05-01 16:02:12 -0600

    Algorand scripts for testing

---
+++ Code/experiments/experiment_scripts/participation_rsm2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export ALGORAND_DATA=/proj/ove-PG0/murray/node/n$1
+echo $ALGORAND_DATA
+~/go/bin/goal node restart -p "10.10.1.13:4161"
+cd /proj/ove-PG0/murray/go-algorand/wallet_app
+node /proj/ove-PG0/murray/go-algorand/wallet_app/wallet_test.js /proj/ove-PG0/murray/go-algorand/wallet_app/node$1.json test$1 > wallet$1.log
