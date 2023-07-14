5382179dae93ee38f0c9dba89eb752a34ee8eda6
Micah Murray <36782608+mmurray22@users.noreply.github.com>
2023-07-14 16:44:17 -0700

    Algorand scripts updated (#23)
    * Updates to algorand scripts, setup script, and experiments json
    
    * Altering setup algorand to include wallet setup
    
    * Setup algorand script complete. Next step is testing
    
    * Almost done with script. Need to add in line to set the algorand data, then test
    
    * Algorand script has been launched! Still an issue with automatically launching the javascript program. All directories hardcoded
    
    * Key bash scripts which collectively setup algorand
    
    * Removes unnecessary script
    
    * It worksssss
    
    * Uncommented all code

--- Code/experiments/experiment_scripts/algorand/shutdown.sh
+++ Code/experiments/experiment_scripts/algorand/shutdown.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+NODE=/proj/ove-PG0/therealmurray/node/dummy_node
 
-export ALGORAND_DATA=/proj/ove-PG0/murray/node/n$1
-echo $ALGORAND_DATA
-~/go/bin/goal node stop
-# ~/go/bin/goal kmd stop
+if [ $1 == "0" ]
+then
+    export ALGORAND_DATA=$NODE/r$2$1
+    echo $ALGORAND_DATA
+    ~/go/bin/goal node stop
+else
+    export ALGORAND_DATA=$NODE/n$2$1 # $1 = node idx
+    echo $ALGORAND_DATA
+    ~/go/bin/goal kmd stop
+    ~/go/bin/goal node stop
+fi
