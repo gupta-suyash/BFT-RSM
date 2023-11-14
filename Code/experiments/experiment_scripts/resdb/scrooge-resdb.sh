a0ce2352dad16db5b5e902d37d660b97a29e05fd
Micah Murray <36782608+mmurray22@users.noreply.github.com>
2023-11-14 12:18:21 -0800

    Update scrooge-resdb.sh after some resdb testing. No acks.

--- Code/experiments/experiment_scripts/resdb/scrooge-resdb.sh
+++ Code/experiments/experiment_scripts/resdb/scrooge-resdb.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-## How to call this scripts: ./scrooge-resdb.sh <dir for resdb> <cluster number>
+## How to call this scripts: ./scrooge-resdb.sh <dir for resdb> <cluster number> <dir for resdb scripts>
+
+echo "PARAMETERS PRINTING NOW!!!!!!!!!!"
+echo $1
+echo $2
+echo $3
+echo "DONE PRINTING PARAMETERS!!!!!!!!!"
 
 echo "Starting ResDB RSM1"
 cd $1/deploy/config
 echo $PWD
-cp rsm$2.conf kv_performance_server.conf
+#cp rsm$2.conf kv_performance_server.conf
 cd ..
 
 echo "Killing old VMs"
@@ -16,7 +22,7 @@ echo "Compiling RSM"
 ./script/deploy.sh ./config/kv_performance_server.conf
 
 echo "Running Bazel Client"
-bazel run //example:kv_server_tools -- $PWD/config_out/client.config set test 1234
-stress -c 3 -m 3
+bazel run //example:kv_server_tools -- $PWD/config_out/client.config set test 1234 &
+#stress -c 3 -m 3
 
 
