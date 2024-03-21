dacecb090d0363c14c42d98f5bb7c5858fd0c400
scrooge node <scrooge@gcloud.com>
2024-03-21 22:36:08 +0000

    Add Inverse Message Scheduler
    Why is this change being made?
    ------------------------------------
    Receivers want to receive duplicate acks from its own cluster to send
    messages which should be significantly delayed. To do this they need to
    know what resend number messages sent to them should be. So now we have
    an Inverse Message Scheduler which can answer this quetsion in O(1)
    after an expensive construction.

---
+++ Code/system/inverse_message_scheduler.cpp
@@ -0,0 +1,58 @@
+#include "inverse_message_scheduler.h"
+
+static uint64_t bitCeil(uint64_t value)
+{
+    value--; // if value is already a power of 2
+    value |= value >> 1; // paint highest bit everywhere
+    value |= value >> 2;
+    value |= value >> 4;
+    value |= value >> 8;
+    value |= value >> 16;
+    value |= value >> 32;
+    value++; // add one to find pow2 > value - 1
+    return value;
+}
+
+InverseMessageScheduler::InverseMessageScheduler(const NodeConfiguration configuration)
+{
+    for (uint64_t curSenderId = 0; curSenderId < configuration.kOtherNetworkSize; curSenderId++)
+    {
+        const auto curSenderConfiguration = NodeConfiguration{
+            .kOwnNetworkSize = configuration.kOtherNetworkSize,
+            .kOtherNetworkSize = configuration.kOwnNetworkSize,
+            .kOwnNetworkStakes = configuration.kOtherNetworkStakes,
+            .kOtherNetworkStakes = configuration.kOwnNetworkStakes,
+            .kOwnMaxNumFailedStake = configuration.kOtherMaxNumFailedStake,
+            .kOtherMaxNumFailedStake = configuration.kOwnMaxNumFailedStake,
+            .kNodeId = curSenderId,
+            .kLogPath = "",
+            .kWorkingDir = ""
+        };
+        const auto curSenderScheduler = MessageScheduler(curSenderConfiguration);
+        const auto messageCycleLength = curSenderScheduler.getMessageCycleLength();
+        
+        mResendNumberLookup.resize(std::max(mResendNumberLookup.size(), messageCycleLength));
+
+        for (uint64_t curMessage{}; curMessage < messageCycleLength; curMessage++)
+        {
+            const auto curMessageResendNum = curSenderScheduler.getResendNumber(curMessage);
+            if (curMessageResendNum.has_value())
+            {
+                const auto initialInsert = not mResendNumberLookup.at(curMessage).has_value();
+                const auto updateValue = mResendNumberLookup.at(curMessage) > curMessageResendNum;
+                if (initialInsert || updateValue)
+                {
+                    mResendNumberLookup.at(curMessage) = curMessageResendNum;
+                }
+            }
+        }
+    }
+    assert("ResendNumberLookup must be a power of 2" && bitCeil(mResendNumberLookup.size()) == mResendNumberLookup.size());
+    kResendNumberLookupBitMask = mResendNumberLookup.size() - 1;
+}
+
+
+std::optional<uint64_t> InverseMessageScheduler::getMinResendNumber(uint64_t sequenceNumber) const
+{
+    return mResendNumberLookup[sequenceNumber % kResendNumberLookupBitMask];
+}
