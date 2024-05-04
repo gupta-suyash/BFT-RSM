b0cd7a39c54844e9472b53314eaae43ecde45345
scrooge node <scrooge@gcloud.com>
2024-05-04 23:42:24 +0000

    rahhh

--- Code/system/config.h
+++ Code/system/config.h
@@ -1,28 +1,31 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
-#define OWN_RSM_SIZE 7
-#define OTHER_RSM_SIZE 7
-#define OWN_RSM_MAX_NODES_FAIL 2
-#define OTHER_RSM_MAX_NODES_FAIL 2
+#define OWN_RSM_SIZE 16
+#define OTHER_RSM_SIZE 16
+#define OWN_RSM_MAX_NODES_FAIL 5
+#define OTHER_RSM_MAX_NODES_FAIL 5
 #define NUMBER_PACKETS 10000
-#define PACKET_SIZE 100
+#define PACKET_SIZE 1000000
 #define NETWORK_DIR "/home/scrooge/BFT-RSM/Code/configuration/"
 #define LOG_DIR "/home/scrooge/BFT-RSM/Code/experiments/results/"
 #define USE_DEBUG_LOGS_BOOL false
 #define WARMUP_TIME 10s
-#define TOTAL_TIME 60s
+#define TOTAL_TIME 40s
 #define BATCH_SIZE 200000
-#define BATCH_CREATION_TIME 1
+#define BATCH_CREATION_TIME 1ms
 #define MAX_NNG_BLOCKING_TIME 500ms
 #define PIPELINE_BUFFER_SIZE 8
 #define MESSAGE_BUFFER_SIZE 256
-#define KLIST_SIZE 0
+#define KLIST_SIZE 64
 #define SCROOGE true
 #define ALL_TO_ALL false
 #define ONE_TO_ONE false
+#define GEOBFT false
+#define LEADER false
 #define FILE_RSM true
-#define NOOP_DELAY .8ms
-#define MAX_MESSAGE_DELAY .8ms
-#define QUACK_WINDOW 100
-#define ACK_WINDOW 30
+#define NOOP_DELAY 1ms
+#define MAX_MESSAGE_DELAY 30ms
+#define QUACK_WINDOW 2000
+#define ACK_WINDOW 19
 #endif
+
