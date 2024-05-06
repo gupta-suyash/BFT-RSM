554ca7b22321913e1b1db412bc7f500dba457e56
scrooge node <scrooge@gcloud.com>
2024-05-06 21:58:45 +0000

    Experiment Generated Commit 2024-05-06_21-58-45/exp-reggie-1/birthday-long

--- Code/makeNetwork.sh
+++ Code/makeNetwork.sh
@@ -23,8 +23,8 @@ username="scrooge"               # TODO: Replace with your username
 workdir="/home/scrooge"
 
 # Set rarely changing Scrooge parameters.
-warmup_time=10s
-total_time=40s
+warmup_time=40s
+total_time=180s
 num_packets=10000
 exec_dir="$HOME/"
 network_dir="${workdir}/BFT-RSM/Code/configuration/"
