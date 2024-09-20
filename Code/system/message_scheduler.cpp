2a560adcb9e8a23e06c993c52f59ca7213a279f5
scrooge node <scrooge@gcloud.com>
2024-09-20 03:04:31 +0000

    Experiment Generated Commit 2024-09-20_03-04-31/exp-reggie-1/huge-stake-sweep

--- Code/system/message_scheduler.cpp
+++ Code/system/message_scheduler.cpp
@@ -365,8 +365,8 @@ MessageScheduler::MessageScheduler(NodeConfiguration configuration)
     const auto stakeInOwnNetwork = message_scheduler::stakeInNetwork(ownNetworkStakePrefixSum);
     const auto stakeInOtherNetwork = message_scheduler::stakeInNetwork(otherNetworkStakePrefixSum);
     const auto networkStakeLcm = std::lcm(stakeInOwnNetwork, stakeInOtherNetwork);
-    const auto ownApportionedStake = std::min<uint64_t>(networkStakeLcm, 1024);
-    const auto otherApportionedStake = std::min<uint64_t>(networkStakeLcm, 1024);
+    const auto ownApportionedStake = networkStakeLcm;
+    const auto otherApportionedStake = networkStakeLcm;
     const auto ownNetworkApportionedStakes =
         message_scheduler::apportionVector(ownApportionedStake, configuration.kOwnNetworkStakes);
     const auto otherNetworkApportionedStakes =
