5a3f8f730c8e6a13c19496a1809654e3d933f991
mmurray22 <micahmurray@berkeley.edu>
2024-11-14 23:53:46 +0000

    Experiment Generated Commit 2024-11-14_23-53-46/micah-raft-redo-benchmark-why/micah-raft-redo-benchmark-why

--- Code/config.h
+++ Code/config.h
@@ -1,11 +1,11 @@
 #ifndef _CONFIG_H_
 #define _CONFIG_H_
-#define OWN_RSM_SIZE 4
-#define OTHER_RSM_SIZE 4
-#define OWN_RSM_MAX_NODES_FAIL 0
-#define OTHER_RSM_MAX_NODES_FAIL 0
+#define OWN_RSM_SIZE 7
+#define OTHER_RSM_SIZE 7
+#define OWN_RSM_MAX_NODES_FAIL 2
+#define OTHER_RSM_MAX_NODES_FAIL 2
 #define NUMBER_PACKETS 10000
-#define PACKET_SIZE 100
+#define PACKET_SIZE 1000000
 #define NETWORK_DIR "/home/scrooge/BFT-RSM/Code/configuration/"
 #define LOG_DIR "/home/scrooge/BFT-RSM/Code/experiments/results/"
 #define USE_DEBUG_LOGS_BOOL false
@@ -16,9 +16,16 @@
 #define MAX_NNG_BLOCKING_TIME 500ms
 #define PIPELINE_BUFFER_SIZE 8
 #define MESSAGE_BUFFER_SIZE 256
-#define KLIST_SIZE 64
+#define KLIST_SIZE 0
 #define SCROOGE true
 #define ALL_TO_ALL false
 #define ONE_TO_ONE false
+#define GEOBFT false
+#define LEADER false
 #define FILE_RSM false
+#define NOOP_DELAY .8ms
+#define MAX_MESSAGE_DELAY .8ms
+#define QUACK_WINDOW 100
+#define ACK_WINDOW 10
 #endif
+
--- Code/system/config.h
+++ Code/system/config.h
@@ -5,14 +5,14 @@
 #define OWN_RSM_MAX_NODES_FAIL 2
 #define OTHER_RSM_MAX_NODES_FAIL 2
 #define NUMBER_PACKETS 10000
-#define PACKET_SIZE 100
+#define PACKET_SIZE 1000000
 #define NETWORK_DIR "/home/scrooge/BFT-RSM/Code/configuration/"
 #define LOG_DIR "/home/scrooge/BFT-RSM/Code/experiments/results/"
 #define USE_DEBUG_LOGS_BOOL false
-#define WARMUP_TIME 10s
-#define TOTAL_TIME 60s
+#define WARMUP_TIME 20s
+#define TOTAL_TIME 120s
 #define BATCH_SIZE 200000
-#define BATCH_CREATION_TIME 1
+#define BATCH_CREATION_TIME 1ms
 #define MAX_NNG_BLOCKING_TIME 500ms
 #define PIPELINE_BUFFER_SIZE 8
 #define MESSAGE_BUFFER_SIZE 256
@@ -22,9 +22,10 @@
 #define ONE_TO_ONE false
 #define GEOBFT false
 #define LEADER false
-#define FILE_RSM true
+#define FILE_RSM false
 #define NOOP_DELAY .8ms
 #define MAX_MESSAGE_DELAY .8ms
 #define QUACK_WINDOW 100
-#define ACK_WINDOW 30
+#define ACK_WINDOW 10
 #endif
+
